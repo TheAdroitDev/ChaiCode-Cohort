84fd5bcbba3eefda5a98dcc6f7a5c1aa22b9c6e3
GitPirates <starshivamverma2@gmail.com>
2025-02-11 08:43:48 +0530

    completed challenge-1 and added md file

---
+++ February-2025/phase-2-JavaScript-Surgeons/week-05/js_Code_Challenges/conditionals_Challenges/challenge-1.js
@@ -0,0 +1,9 @@
+function checkEvenOdd(num) {
+    // Return "Even" if num is even, otherwise return "Odd"
+    if (num % 2 == 0) {
+        return "even";
+    }
+    else{
+        return "odd";
+    }
+}
