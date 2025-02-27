cb797dfe3e8fea2f26ba9f8b9925313d5c07cc2b
GitPirates <starshivamverma2@gmail.com>
2025-02-27 22:13:02 +0530

    adds dom challenges completed all prototypes challenges

---
+++ February-2025/phase-2-JavaScript-Surgeons/week-07/js_Code_Challenges/prototype_Challenges/prototype_Challenge-10.js
@@ -0,0 +1,9 @@
+// You need to implement the Product constructor function and its prototype methods
+
+function Product(name, price, stock) {
+    // Initialize name, price, and stock properties
+}
+
+// Define applyDiscount method on Product's prototype
+
+// Define purchase method on Product's prototype
