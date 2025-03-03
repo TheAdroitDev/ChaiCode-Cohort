0afb4dd27114ac6d12e0325f8ed73eb001d90444
GitPirates <starshivamverma2@gmail.com>
2025-03-03 16:04:37 +0530

    dom challenges 1-2 done added learn file

--- February-2025/phase-2-JavaScript-Surgeons/week-07/js_Code_Challenges/objects_Challenges/objects_Challenge-9.js
+++ February-2025/phase-2-JavaScript-Surgeons/week-07/js_Code_Challenges/objects_Challenges/objects_Challenge-9.js
@@ -1,9 +1,12 @@
 // You just need to implement the deepClone function
 function deepClone(obj) {
-    // Return a deep copy of obj
-    obj = {
-      ...obj
-    }
-    return obj
+  // Return a deep copy of obj
+  if (!obj || typeof obj === undefined) {
+    return obj;
   }
-  
+  let clone = Array.isArray(obj) ? [] : {};
+  for (let key in obj) {
+    clone[key] = deepClone(obj[key])
+  }
+  return clone;
+}
