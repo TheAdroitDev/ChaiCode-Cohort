241eff85dc7f66bdb5b276829b0157a0af204781
GitPirates <starshivamverma2@gmail.com>
2025-04-07 11:19:36 +0530

    fullstack Sql p1 prisma

---
+++ March-2025/phase-3-Node-Ninja/week-10/fullstack_Sql-p1/controller/auth.controller.js
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const registerUser = async (req, res) => {
+    console.log('user registered!');
+    await prisma.user.findUnique({
+        where: { email }
+    })
+}
