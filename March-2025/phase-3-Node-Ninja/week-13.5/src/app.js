4f0d71496975db867bf088aafabdb710d3f140af
GitPirates <starshivamverma2@gmail.com>
2025-09-08 12:35:02 +0530

    fix: api url

--- March-2025/phase-3-Node-Ninja/week-13.5/src/app.js
+++ March-2025/phase-3-Node-Ninja/week-13.5/src/app.js
@@ -1,9 +1,10 @@
-import express from 'express'
+import express from "express";
 
 const app = express();
-// import routes
-import healthCheckRouter from './routes/healthcheck.route';
 
-app.use("app/v1/healthcheck", healthCheckRouter)
+// import routes
+import healthCheckRouter from "./routes/healthcheck.route.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 export default app;
