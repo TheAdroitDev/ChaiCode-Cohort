8d285d6259a430c53961de7e5f5e745ec2a2eaef
GitPirates <starshivamverma2@gmail.com>
2025-09-08 11:10:01 +0530

    feat: implement healthcheck route

--- March-2025/phase-3-Node-Ninja/week-13.5/src/app.js
+++ March-2025/phase-3-Node-Ninja/week-13.5/src/app.js
@@ -1,5 +1,9 @@
 import express from 'express'
 
 const app = express();
+// import routes
+import healthCheckRouter from './routes/healthcheck.route';
+
+app.use("app/v1/healthcheck", healthCheckRouter)
 
 export default app;
