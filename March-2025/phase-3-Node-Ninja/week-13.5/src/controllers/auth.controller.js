fac7ec4bf051160c1f01958bf630b46974953a79
GitPirates <starshivamverma2@gmail.com>
2025-09-08 22:02:19 +0530

    fix: route and controller logic

---
+++ March-2025/phase-3-Node-Ninja/week-13.5/src/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../utils/async-handler.js";
+
+const registerUser = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;
+
+    // validation
+    registrationValidation(body)
+})
+
+export { registerUser }
