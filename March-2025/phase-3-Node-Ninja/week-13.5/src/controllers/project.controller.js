27d4c1bb1c038fe524c7a17abc460280c22cb6fb
GitPirates <starshivamverma2@gmail.com>
2025-09-09 23:39:46 +0530

    feat: setup boilerplate for project controller

---
+++ March-2025/phase-3-Node-Ninja/week-13.5/src/controllers/project.controller.js
@@ -0,0 +1,51 @@
+import { asyncHandler } from "../utils/async-handler.js";
+
+const getProjects = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;  
+
+});
+
+const getProjectById = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;  
+    
+});
+
+const createProject = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;  
+    
+});
+
+const updateProject = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;  
+    
+});
+
+const deleteProject = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;  
+    
+});
+
+const addMemberToProject = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;  
+    
+})
+
+const getProjectMembers = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;  
+    
+})
+
+const updateProjectMembers = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;  
+    
+})
+
+const updateMemberRole = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;  
+    
+})
+
+const deleteMember = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;  
+    
+})
