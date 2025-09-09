ea80e02cacdc9180d3e0d2543d5d3c2cd236dbd8
GitPirates <starshivamverma2@gmail.com>
2025-09-09 23:38:55 +0530

    feat: add boilerplate

--- March-2025/phase-3-Node-Ninja/week-13.5/src/controllers/auth.controller.js
+++ March-2025/phase-3-Node-Ninja/week-13.5/src/controllers/auth.controller.js
@@ -4,7 +4,63 @@ const registerUser = asyncHandler((req, res) => {
     const { email, username, password, role } = req.body;
 
     // validation
-    registrationValidation(body)
+    
+})
+
+const loginUser = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;
+
+    // validation
+
+})
+
+const logoutUser = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;
+
+    // validation
+
+})
+
+const verifyEmail = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;
+
+    // validation
+
+})
+
+const resendVerificationEmail = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;
+
+    // validation
+
+})
+
+const refreshAccessToken = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;
+
+    // validation
+
+})
+
+const forgotPasswordRequest = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;
+
+    // validation
+
+})
+
+const changeCurrentPassword = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;
+
+    // validation
+
+})
+
+const getCurrentUser = asyncHandler((req, res) => {
+    const { email, username, password, role } = req.body;
+
+    // validation
+
 })
 
 export { registerUser }
