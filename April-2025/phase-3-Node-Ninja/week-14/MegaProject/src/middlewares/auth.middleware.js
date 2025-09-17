3fa209a7bbb0c02f78f8bd9e5f9520c5045bd305
GitPirates <starshivamverma2@gmail.com>
2025-09-17 12:39:04 +0530

    fix: complete auth middleware

--- April-2025/phase-3-Node-Ninja/week-14/MegaProject/src/middlewares/auth.middleware.js
+++ April-2025/phase-3-Node-Ninja/week-14/MegaProject/src/middlewares/auth.middleware.js
@@ -1,29 +1,58 @@
+import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiResponse } from "../utils/api-response.js";
+import { ProjectMember } from "../models/projectmember.models.js";
 import { User } from "../models/user.models.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-    const token = req.cookies?.accessToken;
-    req.header("Authorization")?.replace("Bearer ", "");
+    const token = req.cookies?.accessToken || req.header("Authorization").replace("Bearer ", "");
 
-    if (!header) {
-        throw new ApiError(401, "Unauthorized request")
+    // Validation
+    if (!token) {
+        throw new ApiError(401, "Unauthorized request");
     }
 
     try {
-        const decodedToken = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
+        const decodedToken = jwt.verifyJWT(token, process.env.JWT_TOKEN_SECRET);
 
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken  -emailVerificationToken -emailVerificationExpiry");
+        const user = await User.findById(decodedToken?._id).select("-password -emailVerificationExpiry -emailVerificationToken -refreshToken");
 
         if (!user) {
-            throw new ApiErorr(401, "Invalid access token")
+            throw new ApiError(401, "Invalid access token");
         }
 
         req.user = user;
-        next();
+        next()
     } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid access token");
+        throw new ApiError(401, error?.message || "Inavlid access Token")
     }
 })
 
-const validateProjectPermission = asyncHandler
+export const validateProjectPermission = (roles = []) => {
+    asyncHandler(async (req, res) => {
+        const { ProjectId } = req.params;
+
+        if (!ProjectId) {
+            throw new ApiError(404, "Invalid Project");
+        }
+
+        const project = await ProjectMember.find({
+            project: project,
+            user: req.user._id
+        })
+
+        if (!project) {
+            throw new ApiError(404, "Project not found");
+        }
+
+        const givenRole = project?.role;
+
+        req.user.role = givenRole;
+
+        if (!roles.includes(givenRole)) {
+            throw new ApiError(403, "You do not have permission to perform this action")
+        }
+
+        next();
+    })
+}
