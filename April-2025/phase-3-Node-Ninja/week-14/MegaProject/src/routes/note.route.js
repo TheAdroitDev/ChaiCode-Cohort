c3ac42bd2721e1d4364cb4a3d818cd0bb5fdb6a3
GitPirates <starshivamverma2@gmail.com>
2025-09-17 15:51:32 +0530

    feat: setup note route

--- April-2025/phase-3-Node-Ninja/week-14/MegaProject/src/routes/note.route.js
+++ April-2025/phase-3-Node-Ninja/week-14/MegaProject/src/routes/note.route.js
@@ -1,9 +1,14 @@
 import { Router } from "express"
-import { UserRolesEnum } from "../utils/constants.js"
+import { AvailableUserRoles, UserRolesEnum } from "../utils/constants.js"
+import { createNote, deleteNote, getNotes, updateNote } from "../controllers/note.controller.js";
 const router = Router()
 
 router.route("/:projectId")
-    .get(getNotes)
-    .post(validateProjectPermission([UserRolesEnum.ADMIN], UserRolesEnum.MEMBER), createNote)
+    .get(validateProjectPermission(AvailableUserRoles),getNotes)
+    .post(validateProjectPermission([UserRolesEnum.ADMIN]), createNote)
     
+router.route("/:projectId/n/:noteId")
+    .get(validateProjectPermission([AvailableUserRoles],getNotes))
+    .put(validateProjectPermission([UserRolesEnum.ADMIN],updateNote))
+    .delete(validateProjectPermission([UserRolesEnum.ADMIN],deleteNote))
 export default router;
