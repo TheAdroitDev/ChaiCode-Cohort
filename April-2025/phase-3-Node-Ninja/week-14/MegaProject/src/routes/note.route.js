f0a0460d71bcc7c86a24414405e51ce72e8292dc
GitPirates <starshivamverma2@gmail.com>
2025-09-17 12:41:05 +0530

    fix: note post request params

--- April-2025/phase-3-Node-Ninja/week-14/MegaProject/src/routes/note.route.js
+++ April-2025/phase-3-Node-Ninja/week-14/MegaProject/src/routes/note.route.js
@@ -4,6 +4,6 @@ const router = Router()
 
 router.route("/:projectId")
     .get(getNotes)
-    .post(validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER]), createNote)
+    .post(validateProjectPermission([UserRolesEnum.ADMIN], UserRolesEnum.MEMBER), createNote)
     
 export default router;
